ee61f77299b076231fc646faaf6cdb6e480c33bf
Sahil Kanungo <76621577+sahil0017@users.noreply.github.com>
2021-02-22 11:05:40 +0530

    Priority Queue using Linked List

--- MainQueue.java
+++ MainQueue.java
@@ -1,121 +1,74 @@
-package dataStructures;
+package PriorityQueue;
 
 public class MainQueue {
 	
-	//static class for Node creation
 	static class Node
 	{
 		int data;
+		int priority;
 		Node next;
-		//Node rear,front;
-		//int length=0;
 		
-		//constructor 
-		Node(int data)
+		public Node(int  data,int priority)
 		{
 			this.data=data;
-			this.next=null;
+			this.priority=priority;
+			this.next=null;			
 		}
-		
 	}
+	static Node head;
 	
-	//instances of Queue
-	static Node rear=null;
-	static Node front=null;
-	//static int length;
-	
-	//METHOD TO add the element in FIFO
-    void enqueue(int data)
+	static Node push(Node head,int d,int p)
 	{
-		Node temp=new Node(data);
-		System.out.printf("Inserting element %d\n",data );
+		Node start =head;
+		Node temp=new Node(d,p);
 		
-		//IF THE QUEUE IS EMPTY THE ELEMENT IS ADDED AT THE FRONT . SO FRONT AND REAR NODE POINTS TO THE ADDED NODE 
-		if(front==null)
+		if(head.priority>p)
 		{
-			front=temp;
-			rear=temp;
+			temp.next=head;
+			head=temp;
 		}
-		//IF SOME NODES ARE INSERTED INTO QUEUE EARLIER THEN REAR NEXT POINTS TO NEW NODE AND MAKE THE NEW NODE AS REAR
-		//UPDATE THE REAR NODES.
-		//FROM THIS WE LEARN REAR NODES EXECUTES INSERTION.
 		else
 		{
-			rear.next=temp;
-			rear=temp;
-		}		
+			while(start.next!=null && start.next.priority<p)
+			{
+				start=start.next;
+			}
+			temp.next=start.next;
+			start.next=temp;
+		}
+		return head;
 	}
-    //METHOD TO REMOVE THE ELEMENT
-    static void dequeue()
-    {
-    	//CHECK FOR UNDERFLOW
-    	if(front==null)
-    	{
-    		System.out.println("UnderFlow Condition");
-    		System.exit(0);
-    	}
-    	
-    	// VARIABLE NODE TO  STORES THE FRONT 
-    	//PRINT THE FRONT DATA
-    	//.UPDATE THE FRONT NODE
-    	Node temp=front;
-    	System.out.printf("Removing element %d\n ",temp.data);
-    	front=front.next;
-    	
-    	//AFTER UPDATION CHECK WHETHER FRONT NODE REACHES TO REAR NODE WHERE REAR NODE .NEXT IS NULL
-    	//INITIAL FRONT AS NULL AND REAR AS NULL
-    	//HERE THE QUEUE IS EMPTY . 
-    	if(front==null)
-    		rear=null;
-    	
-    //	int item=temp.data;
-    //	return item;
-    }
-    
-    //METHOD TO FIND THE PEEK VALUE 
-    static void peek()
-    {
-    	//CHECK IF THE FRONT AND REAR POINTS TO NULL
-    	// ie QUEUE IS EMPTY AND RETURN 
-    	if(front==null && rear==null)
-    	{
-    		System.out.println(" NO ELEMENT IN THE QUEUE");
-    		System.exit(0);
-    	}
-    	//PEEK VALUE OF THE QUEUE
-    	System.out.printf("Peek Element at this moment %d\n",front.data);
-    	//return front.data;
-    	
-    }
-    
-    //METHOD TO CHECK WHETHER QUEUE IS EMPTY OR NOT
-	static boolean isEmpty()
+	
+	static boolean isEmpty(Node head)
 	{
-		return front==null && rear==null;
+		return head==null;
 	}
+	
+	static int peek(Node head)
+	{
+		return head.data;
+	}
+	
+	static void pop()
+	{
+		Node temp=head;
+		System.out.println("Deleted Node is  "+temp.data);
+		head=head.next;
+		//return head;
+	}
+	
 
 	public static void main(String[] args) {
-		MainQueue queue=new MainQueue();
+		Node pq=new Node(4,1);
+		pq=push(pq,5,2);
+		pq=push(pq,7,8);
+		pq=push(pq,2,3);
 		
-		queue.enqueue(34);
-		queue.enqueue(343);
-		queue.enqueue(341);
-		queue.enqueue(314);
-		queue.enqueue(134);
-		
-		dequeue();
-		dequeue();
-		peek();
-		//System.out.println("PEEK ELEMENT IN THE QUEUE "+peek());
-		//queue.dequeue();
-		//queue.dequeue();
-		//queue.dequeue();
-		
-		//CHECK WHETHER THE QUEUE IS EMPTY OR NOT
-		if(isEmpty())
-			System.out.println("Queue is empty ");
-		else
-			System.out.println(" Queue is not empty");
+		while(!isEmpty(pq))
+		{
+			System.out.printf("%d",peek(pq));
+			
+		}
 
 	}
 
